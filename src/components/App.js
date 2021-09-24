8e6e86d899d77aae993c69ba22e4901e37e6d3c5
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-24 09:48:56 +0200

    update structure Html and JS

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,73 @@
 import '../styles/App.scss';
 import { useEffect, useState } from 'react';
-import data from '../services/data.json';
+import dataJson from '../services/data.json';
 import ls from '../services/local-storage.js'; //localStorage
 
-const handleButton = (ev) => {
-  ev.preventDefault();
-};
-
-const renderClubs = () => {
-  debugger;
-  console.log(data);
-  data.map((el, i) => {
-    return (
-      <li key={i} className="li_container">
-        <h3>
-          #{i} {el.name}
-          <button onClick={handleButton} className="buttonX">
-            x
-          </button>
-        </h3>
-        <p>
-          <strong>Abierto entre semana: </strong>
-          <small>{el.openOnWeekdays ? 'Si' : 'No'}</small>
-          <strong>Abierto el fin de semana: </strong>
-          <small>{el.openOnWeekend ? 'Si' : 'No'}</small>
-        </p>
-      </li>
-    );
-  });
-};
-
 function App() {
+  //STATES-------------------------------------------------------------------------------------
+  const [data, setData] = useState(dataJson);
+  const [newData, setNewData] = useState({
+    name: '',
+    openOnWeekdays: true,
+    openOnWeekend: true,
+  });
+
+  //HANDLE-----------------------------------------------------------------------------------------
+  //pending
+  const handleButton = (ev) => {
+    ev.preventDefault();
+  };
+
+  //pending comprobation
+  const handleCheckWeek = (ev) => {
+    if (ev.currentTarget.checked) {
+      setNewData({ ...newData, openOnWeekdays: true });
+    } else {
+      setNewData({ ...newData, openOnWeekdays: false });
+    }
+  };
+  const handleCheckWeekend = (ev) => {
+    if (ev.currentTarget.checked) {
+      setNewData({ ...newData, openOnWeekdays: true });
+    } else {
+      setNewData({ ...newData, openOnWeekdays: false });
+    }
+  };
+  const handleButtonAdd = (ev) => {
+    ev.preventDefault();
+    data.push(newData);
+    setData([...data]);
+  };
+  //ok
+  const handleInput = (ev) => {
+    setNewData({ ...newData, name: ev.currentTarget.value });
+  };
+
+  //RENDER---------------------------------------------------------------------------------------------
+  //useEffect?
+  const renderClubs = () => {
+    console.log(data);
+    data.map((el, i) => {
+      return (
+        <li key={i} className="li_container">
+          <h3>
+            #{i} {el.name}
+            <button onClick={handleButton} className="buttonX">
+              x
+            </button>
+          </h3>
+          <p>
+            <strong>Abierto entre semana: </strong>
+            <small>{el.openOnWeekdays ? 'Si' : 'No'}</small>
+            <strong>Abierto el fin de semana: </strong>
+            <small>{el.openOnWeekend ? 'Si' : 'No'}</small>
+          </p>
+        </li>
+      );
+    });
+  };
+
+  //HTML----------------------------------------------------------------------
   return (
     <div>
       <header>
@@ -55,6 +92,8 @@ function App() {
                 name="name"
                 id="name"
                 placeholder="Nombre del club..."
+                value={newData.name}
+                onChange={handleInput()}
               />
             </label>
             <label htmlFor="weekOpen">
@@ -63,6 +102,7 @@ function App() {
                 type="checkbox"
                 name="weekOpen"
                 id="weekOpen"
+                onChange={handleCheckWeek()}
                 checked={true}
               />
             </label>
@@ -72,10 +112,13 @@ function App() {
                 type="checkbox"
                 name="weekendOpen"
                 id="weekendOpen"
+                onChange={handleCheckWeekend()}
                 checked={false}
               />
             </label>
-            <button className="submitBtn">Añadir un nuevo club</button>
+            <button className="submitBtn" onClick={handleButtonAdd()}>
+              Añadir un nuevo club
+            </button>
           </form>
         </section>
       </main>
